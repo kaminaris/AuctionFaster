a500caff9e9b2072692add38c0c9688d9cb2890d
Pawel <zacdbg@gmail.com>
2018-04-22 00:21:59 +0200

    initial searching AH

--- main.lua
+++ main.lua
@@ -39,6 +39,7 @@ function AuctionFaster:OnInitialize()
 	--	self:RegisterChatCommand('keyprint', 'PrintKeystone');
 	self:RegisterEvent('BAG_UPDATE_DELAYED');
 	self:RegisterEvent('AUCTION_HOUSE_SHOW');
+	self:RegisterEvent('AUCTION_ITEM_LIST_UPDATE');
 end
 
 function AuctionFaster:AddAuctionHouseTab()
@@ -68,79 +69,25 @@ function AuctionFaster:AddAuctionHouseTab()
 
 	self.TabAdded = true;
 
-	local closeBtn = CreateFrame('Button', nil, auctionTab, 'UIPanelButtonTemplate')
-	closeBtn:SetPoint('BOTTOMRIGHT', -5, 5)
-	closeBtn:SetWidth(75)
-	closeBtn:SetHeight(24)
-	closeBtn:SetText('dziaua')
-
-
-	local cols = {
-		{
-			['name'] = 'Icon',
-			['width'] = 30,
-			['align'] = 'LEFT',
-		},
-
-		{
-			['name'] = 'Name',
-			['width'] = 120,
-			['align'] = 'LEFT',
-		},
-
-		{
-			['name'] = 'Price',
-			['width'] = 80,
-			['align'] = 'RIGHT',
-		},
-	}
-	--auctionTab.scrollTable = ScrollingTable:CreateST(cols, 16, 20, nil, auctionTab);
-
 	self:ScanInventory();
 	--self:UpdateInventoryTable();
 	self:DrawItemsFrame(auctionTab);
-
-
+	self:DrawRightPane(auctionTab);
+	self:DrawTabButtons();
 
 	self:Hook('AuctionFrameTab_OnClick', true);
 end
 
 function AuctionFaster:DrawItemsFrame(auctionTab)
-	auctionTab.scrollframe = CreateFrame('ScrollFrame', 'ANewScrollFrame', auctionTab, 'UIPanelScrollFrameTemplate');
-	auctionTab.scrollframe:SetSize(300, 300);
-	auctionTab.scrollframe:SetPoint('TOPLEFT', 5, -25);
-	auctionTab.scrollframe:SetPoint('BOTTOMLEFT', 300, 35);
+	local scrollFrame, scrollChild = StdUi:ScrollFrame(auctionTab, 'AFLeftScroll', 300, 300);
+	scrollFrame:SetPoint('TOPLEFT', 5, -25);
+	scrollFrame:SetPoint('BOTTOMLEFT', 300, 35);
 
-	local scrollBar = _G['ANewScrollFrameScrollBar'];
-
-	local tex = auctionTab.scrollframe:CreateTexture(nil, 'BACKGROUND', nil, -6)
-	tex:SetPoint('TOP', auctionTab.scrollframe)
-	tex:SetPoint('RIGHT', scrollBar, 3.7, 0)
-	tex:SetPoint('BOTTOM', auctionTab.scrollframe)
-	tex:SetWidth(scrollBar:GetWidth() + 10)
-	tex:SetTexture('Interface\\PaperDollInfoFrame\\UI-Character-ScrollBar')
-	tex:SetTexCoord(0, 0.45, 0.1640625, 1)
-
-	local scrollChild = CreateFrame('Frame', '$parentScrollChild1', auctionTab.scrollframe)
-	scrollChild:SetWidth(auctionTab.scrollframe:GetWidth())
-	scrollChild:SetHeight(900);
-
-	auctionTab.scrollframe:SetScrollChild(scrollChild)
-	auctionTab.scrollframe:EnableMouse(true)
-
-	--make sure you cannot move the panel out of the screen
-	auctionTab.scrollframe:SetClampedToScreen(true)
+	auctionTab.scrollFrame = scrollFrame;
+	auctionTab.scrollChild = scrollChild;
 
 	self:DrawItems(scrollChild);
-	self:DrawRightPane(auctionTab);
-end
 
-function AuctionFaster:HighlightFrame(frame, highlight)
-	if highlight then
-		frame.highlightTexture:Show()
-	else
-		frame.highlightTexture:Hide()
-	end
 end
 
 function AuctionFaster:ShowTooltip(frame, link, show)
@@ -160,53 +107,34 @@ function AuctionFaster:DrawItems(scrollChild)
 	scrollChild:SetHeight(lineHeight * #self.inventoryItems);
 
 	for i = 1, #self.inventoryItems do
-		local holdingFrame = CreateFrame('Button', 'ASScrollItem' .. i, scrollChild);
-		holdingFrame:SetPoint('TOPLEFT', 0, -(i - 1) * lineHeight);
-		holdingFrame:SetSize(scrollChild:GetWidth(), lineHeight);
 
-		holdingFrame.highlightTexture = holdingFrame:CreateTexture(nil, 'BACKGROUND')
-		holdingFrame.highlightTexture:SetAllPoints(true)
-		holdingFrame.highlightTexture:SetColorTexture(1, 0.9, 0, 0.5)
-		holdingFrame.highlightTexture:Hide();
+		local holdingFrame = StdUi:Button(scrollChild, scrollChild:GetWidth(), lineHeight, nil, false, true, false);
+		holdingFrame.highlightTexture:SetColorTexture(1, 0.9, 0, 0.5);
+		holdingFrame:SetPoint('TOPLEFT', 0, -(i - 1) * lineHeight);
 		holdingFrame.itemLink = self.inventoryItems[i].link;
 		holdingFrame.itemIndex = i;
-
 
 		holdingFrame:EnableMouse();
 		holdingFrame:RegisterForClicks('AnyUp');
 		holdingFrame:SetScript('OnEnter', function(self)
-			AuctionFaster:HighlightFrame(self, true);
 			AuctionFaster:ShowTooltip(self, self.itemLink, true);
 		end)
 		holdingFrame:SetScript('OnLeave', function(self)
-			AuctionFaster:HighlightFrame(self, false);
 			AuctionFaster:ShowTooltip(self, nil, false);
 		end)
 		holdingFrame:SetScript('OnClick', function(self)
 			AuctionFaster:SelectItem(self.itemIndex);
 		end)
 
-
-		local texx = holdingFrame:CreateTexture(nil, 'ARTWORK')
+		local texx = StdUi:Texture(holdingFrame, lineHeight - 2, lineHeight - 2, self.inventoryItems[i].icon);
 		texx:SetPoint('TOPLEFT', holdingFrame, 1, -1)
-		texx:SetSize(lineHeight - 2, lineHeight - 2);
-		texx:SetTexture(self.inventoryItems[i].icon)
 
-		local name = holdingFrame:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
-		name:SetText(self.inventoryItems[i].name);
-		name:SetSize(150, 20);
+		local name = StdUi:Label(holdingFrame, nil, self.inventoryItems[i].name, nil, 150, 20);
 		name:SetPoint('TOPLEFT', 35, -5);
-		name:SetJustifyH('LEFT');
 
-		local price = StdUi:Label(holdingFrame, nil, GetMoneyString(self.inventoryItems[i].price), nil, 80, 20);
+		local price = StdUi:Label(holdingFrame, nil, AuctionFaster:FormatMoney(self.inventoryItems[i].price), nil, 80, 20);
 		price:SetJustifyH('RIGHT');
 		price:SetPoint('TOPRIGHT', 0, -5);
-
---		local price = holdingFrame:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
---		price:SetText(GetMoneyString(self.inventoryItems[i].price));
---		price:SetSize(80, 20);
---		price:SetPoint('TOPRIGHT', holdingFrame, 0, -5);
---		price:SetJustifyH('RIGHT');
 	end
 end
 
@@ -214,31 +142,101 @@ function AuctionFaster:SelectItem(index)
 	local auctionTab = self.auctionTab;
 	self.selectedItem = self.inventoryItems[index];
 
-
 	auctionTab.itemIcon:SetTexture(self.selectedItem.icon);
 	auctionTab.itemName:SetText(self.selectedItem.name);
+	auctionTab.itemQty:SetText('Qty: ' .. self.selectedItem.count);
+
+	self:GetCurrentAuctions();
 end
 
 function AuctionFaster:DrawRightPane(auctionTab)
 	local leftMargin = 340;
 	local iconSize = 48;
-	auctionTab.itemIcon = auctionTab:CreateTexture(nil, 'ARTWORK')
+
+	local rightMargin = leftMargin + 200;
+
+	auctionTab.itemIcon = StdUi:Texture(auctionTab, iconSize, iconSize, '');
 	auctionTab.itemIcon:SetPoint('TOPLEFT', leftMargin, -25)
-	auctionTab.itemIcon:SetSize(iconSize, iconSize);
-	auctionTab.itemIcon:SetTexture('')
 
-	auctionTab.itemName = auctionTab:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
-	auctionTab.itemName:SetText('No item selected');
-	auctionTab.itemName:SetSize(300, 20);
+	auctionTab.itemName = StdUi:Label(auctionTab, 16, 'No item selected', nil, 300, 20);
 	auctionTab.itemName:SetPoint('TOPLEFT', leftMargin + iconSize + 5, -25);
-	auctionTab.itemName:SetJustifyH('LEFT');
 
-	auctionTab.itemQty = auctionTab:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
-	auctionTab.itemName:SetText('No item selected');
-	auctionTab.itemName:SetSize(300, 20);
-	auctionTab.itemName:SetPoint('TOPLEFT', leftMargin + iconSize + 5, -25);
-	auctionTab.itemName:SetJustifyH('LEFT');
+	auctionTab.itemQty = StdUi:Label(auctionTab, 14, 'Qty: O', nil, 300, 20);
+	auctionTab.itemQty:SetPoint('TOPLEFT', leftMargin + iconSize + 5, -45);
 
+
+	-- Bid per item edit box
+	auctionTab.bidPerItem = StdUi:EditBoxWithLabel(auctionTab, 150, 20, '10g 6s 6c',
+		'Bid Per Item', nil, 'TOP');
+	StdUi:GlueBelow(auctionTab.bidPerItem, auctionTab.itemIcon, 0, -25);
+
+	-- Buy per item edit box
+	auctionTab.buyPerItem = StdUi:EditBoxWithLabel(auctionTab, 150, 20, '12g 10s 10c',
+		'Buy Per Item', nil, 'TOP');
+	StdUi:GlueBelow(auctionTab.buyPerItem, auctionTab.bidPerItem, 0, -25);
+
+
+	-- Stack Size
+	auctionTab.stackSize = StdUi:EditBoxWithLabel(auctionTab, 150, 20, '1',
+		'Stack Size (Max: 20)', nil, 'TOP');
+	auctionTab.stackSize:SetNumeric(true);
+	StdUi:GlueRight(auctionTab.stackSize, auctionTab.bidPerItem, 120, 0);
+
+	auctionTab.maxStacks = StdUi:EditBoxWithLabel(auctionTab, 150, 20, '-1',
+		'Limit of stacks (-1 means everything)', nil, 'TOP');
+	auctionTab.maxStacks:SetNumeric(true);
+	StdUi:GlueRight(auctionTab.maxStacks, auctionTab.buyPerItem, 120, 0);
+
+	self:DrawRightPaneCurrentAuctionsTable();
+end
+
+function AuctionFaster:DrawTabButtons()
+	local auctionTab = self.auctionTab;
+
+	auctionTab.postButton = StdUi:PanelButton(auctionTab, 60, 20, 'Post All');
+	auctionTab.postButton:SetPoint('BOTTOMRIGHT', -5, 5)
+
+	auctionTab.postOneButton = StdUi:PanelButton(auctionTab, 60, 20, 'Post One');
+	StdUi:GlueLeft(auctionTab.postOneButton, auctionTab.postButton, 5);
+
+	auctionTab.buyItemButton = StdUi:PanelButton(auctionTab, 60, 20, 'Buy Item');
+	StdUi:GlueLeft(auctionTab.buyItemButton, auctionTab.postOneButton, 5);
+end
+
+function AuctionFaster:DrawRightPaneCurrentAuctionsTable()
+	local auctionTab = self.auctionTab;
+
+
+	local cols = {
+		{
+			['name'] = 'Seller',
+			['width'] = 150,
+			['align'] = 'LEFT',
+		},
+
+		{
+			['name'] = 'Qty',
+			['width'] = 40,
+			['align'] = 'LEFT',
+		},
+
+		{
+			['name'] = 'Bid',
+			['width'] = 120,
+			['align'] = 'RIGHT',
+		},
+
+		{
+			['name'] = 'Buy',
+			['width'] = 120,
+			['align'] = 'RIGHT',
+		},
+	}
+
+	local leftMargin = 340;
+	auctionTab.currentAuctions = ScrollingTable:CreateST(cols, 12, 18, nil, auctionTab);
+	auctionTab.currentAuctions.frame:SetPoint('TOPLEFT', leftMargin, -200);
+	auctionTab.currentAuctions.frame:SetPoint('BOTTOMRIGHT', 0, 25);
 end
 
 function AuctionFaster:AuctionFrameTab_OnClick(tab)
@@ -249,18 +247,38 @@ function AuctionFaster:AuctionFrameTab_OnClick(tab)
 	end
 end
 
-function AuctionFaster:UpdateInventoryTable()
-	local tableData = {};
+function AuctionFaster:GetCurrentAuctions()
+	local selectedItem = self.selectedItem;
+	local itemId = selectedItem.itemId;
+	local name = selectedItem.name;
 
-	for char, item in pairs(self.inventoryItems) do
+	if not CanSendAuctionQuery() then
+		print('cant wut');
+		return;
+	end
+
+	local result = QueryAuctionItems(name, nil, nil, 0, 0, 0, false, true);
+end
+
+function AuctionFaster:AUCTION_ITEM_LIST_UPDATE()
+
+	local shown, total = GetNumAuctionItems('list');
+
+	local tableData = {};
+	for i = 1, shown do
+		local name, texture, count, quality, canUse, level, levelColHeader, minBid,
+		minIncrement, buyoutPrice, bidAmount, highBidder, bidderFullName, owner,
+		ownerFullName, saleStatus, itemId, hasAllInfo = GetAuctionItemInfo('list', i);
+
 		tinsert(tableData, {
-			item.icon,
-			item.name,
-			GetMoneyString(item.price),
+			owner,
+			count,
+			GetMoneyString(floor(minBid / count)),
+			GetMoneyString(floor(buyoutPrice / count)),
 		});
 	end
 
-	self.auctionTab.scrollTable:SetData(tableData, true);
+	self.auctionTab.currentAuctions:SetData(tableData, true);
 end
 
 function AuctionFaster:ScanInventory()
@@ -277,14 +295,15 @@ function AuctionFaster:ScanInventory()
 			for slot = 1, numSlots do
 				local itemId = GetContainerItemID(bag, slot);
 				local link = GetContainerItemLink(bag, slot);
+				local _, count = GetContainerItemInfo(bag, slot);
 
-				self:AddItemToInventory(itemId, link, bag, slot);
+				self:AddItemToInventory(itemId, count, link, bag, slot);
 			end
 		end
 	end
 end
 
-function AuctionFaster:AddItemToInventory(itemId, link, bag, slot)
+function AuctionFaster:AddItemToInventory(itemId, count, link, bag, slot)
 	local canSell = false;
 
 	if itemId == 82800 then
@@ -324,34 +343,51 @@ function AuctionFaster:AddItemToInventory(itemId, link, bag, slot)
 
 	local itemIcon = GetItemIcon(itemId);
 
-	tinsert(self.inventoryItems, {
-		icon = itemIcon,
-		count = itemStackCount,
-		name = itemName,
-		link = itemLink,
-		price = itemSellPrice or 0
-	});
+	local found = false;
+	for i = 1, #self.inventoryItems do
+		local ii = self.inventoryItems[i];
+		if ii.itemId == itemId and ii.name == itemName then
+			found = true;
+			self.inventoryItems[i].count = self.inventoryItems[i].count + count;
+			break;
+		end
+	end
+
+	if not found then
+		tinsert(self.inventoryItems, {
+			icon = itemIcon,
+			count = count,
+			maxStack = itemStackCount,
+			name = itemName,
+			link = itemLink,
+			itemId = itemId,
+			price = itemSellPrice or 0
+		});
+	end
 end
 
 function AuctionFaster:FormatMoney(money)
 
-	local money = tonumber(money)
+	local money = tonumber(money);
+	local goldColor = '|cfffff209';
+	local silverColor = '|cff7b7b7a';
+	local copperColor = '|cffac7248';
 
-	local gold = floor(money / COPPER_PER_GOLD)
-	local silver = floor((money - (gold * COPPER_PER_GOLD)) / COPPER_PER_SILVER)
-	local copper = floor(money % COPPER_PER_SILVER)
+	local gold = floor(money / COPPER_PER_GOLD);
+	local silver = floor((money - (gold * COPPER_PER_GOLD)) / COPPER_PER_SILVER);
+	local copper = floor(money % COPPER_PER_SILVER);
 
 	local output = '';
 
 	if gold > 0 then
-		output = format('%s%s ', color .. gold .. '|r', 'g')
+		output = format('%s%s ', goldColor .. gold .. '|r', 'g')
 	end
 
 	if gold > 0 or silver > 0 then
-		output = format('%s%s%s ', output, color .. silver .. '|r', 's')
+		output = format('%s%s%s ', output, silverColor .. silver .. '|r', 's')
 	end
 
-	output = format('%s%s%s ', output, color .. copper .. '|r', 'c')
+	output = format('%s%s%s ', output, copperColor .. copper .. '|r', 'c')
 
 	return output:trim();
 end
--- ui.lua
+++ ui.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "StdUi-1.0", 1;
+local MAJOR, MINOR = 'StdUi-1.0', 1;
 local StdUi = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not StdUi then
@@ -17,21 +17,179 @@ function StdUi:SetDefaultFont(font, size)
 	self.config.fontSize = size;
 end
 
+--
+-- Conditionally set object width and height
+--
+function StdUi:SetObjSize(obj, width, height)
+	if width then
+		obj:SetWidth(width);
+	end
+
+	if height then
+		obj:SetHeight(height);
+	end
+end
+
+--
+-- Positioning functions
+--
+function StdUi:GlueBelow(object, referencedObject, x, y)
+	object:SetPoint('TOPLEFT', referencedObject, 'BOTTOMLEFT', x, y);
+end
+
+function StdUi:GlueAbove(object, referencedObject, x, y)
+	object:SetPoint('BOTTOMLEFT', referencedObject, 'TOPLEFT', x, y);
+end
+
+function StdUi:GlueTopRight(object, referencedObject, x, y)
+	object:SetPoint('TOPLEFT', referencedObject, 'BOTTOMLEFT', x, y);
+end
+
+function StdUi:GlueRight(object, referencedObject, x, y)
+	object:SetPoint('LEFT', referencedObject, 'RIGHT', x, y);
+end
+
+function StdUi:GlueLeft(object, referencedObject, x, y)
+	object:SetPoint('RIGHT', referencedObject, 'LEFT', x, y);
+end
+
 function StdUi:Label(parent, size, text, inherit, width, height)
 
 	local fs = parent:CreateFontString(nil, self.config.fontStrata, inherit);
 
 	fs:SetFont(self.config.font, size or self.config.fontSize, self.config.fontEffect);
 	fs:SetText(text);
-	if width then
-		fs:SetWidth(width);
-	end
-	if height then
-		fs:SetHeight(height);
-	end
+	self:SetObjSize(fs, width, height);
 
 	fs:SetJustifyH('LEFT');
 	fs:SetJustifyV('MIDDLE');
 
 	return fs;
 end
+
+function StdUi:Texture(parent, width, height, texture)
+	local tex = parent:CreateTexture(nil, 'ARTWORK');
+
+	self:SetObjSize(tex, width, height);
+	tex:SetTexture(texture);
+
+	return tex;
+end
+
+function StdUi:PanelButton(parent, width, height, text)
+	local button = CreateFrame('Button', nil, parent, 'UIPanelButtonTemplate');
+	self:SetObjSize(button, width, height);
+	if text then
+		button:SetText(text);
+	end
+
+	return button;
+end
+
+function StdUi:Button(parent, width, height, text, normalTexture, highlightTexture, pushTexture)
+	local button = CreateFrame('Button', nil, parent)
+	self:SetObjSize(button, width, height);
+
+	if text then
+		button:SetText(text);
+		button:SetNormalFontObject('GameFontNormal');
+	end
+
+	if normalTexture then
+		local normTex = 'Interface/Buttons/UI-Panel-Button-Up';
+		if normalTexture ~= true then
+			normTex = normalTexture;
+		end
+
+		local ntex = self:Texture(button, nil, nil, normTex);
+		ntex:SetTexCoord(0, 0.625, 0, 0.6875);
+		ntex:SetAllPoints();
+
+		button:SetNormalTexture(ntex);
+		button.normalTexture = ntex;
+	end
+
+	if highlightTexture then
+		local highTex = 'Interface/Buttons/UI-Panel-Button-Highlight';
+		if highlightTexture ~= true then
+			highTex = highlightTexture;
+		end
+		local htex = self:Texture(button, nil, nil, highTex);
+		htex:SetTexCoord(0, 0.625, 0, 0.6875)
+		htex:SetAllPoints();
+		button:SetHighlightTexture(htex);
+		button.highlightTexture = htex;
+	end
+
+	if pushTexture then
+		local pushTex = 'Interface/Buttons/UI-Panel-Button-Down';
+		if pushTexture ~= true then
+			pushTex = pushTexture;
+		end
+		local ptex = self:Texture(button, nil, nil, pushTex);
+
+		ptex:SetTexCoord(0, 0.625, 0, 0.6875)
+		ptex:SetAllPoints()
+		button:SetPushedTexture(ptex)
+		button.pushedTexture = ptex;
+	end
+
+	return button;
+end
+
+function StdUi:EditBox(parent, width, height, text)
+	local editBox = CreateFrame('EditBox', nil, parent, 'InputBoxTemplate');
+	editBox:SetAutoFocus(false);
+	self:SetObjSize(editBox, width, height);
+	if text then
+		editBox:SetText(text);
+	end
+
+	return editBox;
+end
+
+function StdUi:EditBoxWithLabel(parent, width, height, text, label, labelFontSize, labelPosition, labelWidth)
+	local editBox = CreateFrame('EditBox', nil, parent, 'InputBoxTemplate');
+	editBox:SetAutoFocus(false);
+	self:SetObjSize(editBox, width, height);
+	if text then
+		editBox:SetText(text);
+	end
+
+	local labelHeight = (labelFontSize or 12) + 4;
+	local label = self:Label(parent, labelFontSize or 12, label, nil, labelWidth, labelHeight);
+
+	if labelPosition == 'TOP' or labelPosition == nil then
+		self:GlueAbove(label, editBox, 0, 4)
+	else -- labelPosition == 'LEFT'
+		label:SetWidth(labelWidth or label:GetStringWidth())
+		self:GlueLeft(label, editBox, 4, 0);
+	end
+
+	editBox.label = label;
+
+	return editBox;
+end
+
+function StdUi:ActionButton(parent)
+
+end
+
+
+function StdUi:ScrollFrame(parent, name, width, height)
+	local scrollFrame = CreateFrame('ScrollFrame', name, parent, 'UIPanelScrollFrameTemplate');
+	scrollFrame:SetSize(width, height);
+
+	local scrollBar = _G[name .. 'ScrollBar'];
+
+	local scrollChild = CreateFrame('Frame', name .. 'ScrollChild', scrollFrame);
+	scrollChild:SetWidth(scrollFrame:GetWidth());
+	scrollChild:SetHeight(scrollFrame:GetHeight());
+
+	scrollFrame:SetScrollChild(scrollChild);
+	scrollFrame:EnableMouse(true);
+	scrollFrame:SetClampedToScreen(true);
+
+	return scrollFrame, scrollChild, scrollBar;
+end
+
